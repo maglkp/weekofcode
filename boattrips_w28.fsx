5897308de0c8d6acb9e7a93a9b8e0b5d22e36bd7
Lukasz Koziol <maglkp@gmail.com>
2017-01-09 10:22:22 +0100

    add boat simple

---
+++ boattrips_w28.fsx
@@ -0,0 +1,14 @@
+﻿open System
+
+let (n, c, m, ps) = 
+    let firstLine = Console.ReadLine().Split(' ')
+    let (n, c, m) = (Int32.Parse(firstLine.[0]), Int32.Parse(firstLine.[1]), Int32.Parse(firstLine.[2]))
+    let secondLine = Console.ReadLine()
+    let ps = secondLine.Split(' ') |> Array.map Int32.Parse |> List.ofArray
+    (n, c, m, ps)
+
+let cap = c * m
+if List.forall (fun e -> e <= cap) ps then 
+    printfn "%s" "Yes"
+else 
+    printfn "%s" "No"
