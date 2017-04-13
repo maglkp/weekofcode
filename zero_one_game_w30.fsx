6b1c5eb179c23f06cb232a9d02e2944c57263c75
Lukasz Koziol <maglkp@gmail.com>
2017-04-13 09:03:36 +0200

    w30 add zero sum

--- zero_one_game_w30.fsx
+++ zero_one_game_w30.fsx
@@ -5,18 +5,34 @@ let arrays =
     List.init cnt (fun i -> 
                             let _ = Console.ReadLine()
                             Console.ReadLine().Split(' ') |> Array.map Int32.Parse |> List.ofArray) 
-let rec shorten xs =
-    let except (xs: int list) (ix:int) = 
+
+let rec shorten (xs: int list) =
+    //printfn "%A" xs
+    let except (xs: int list list) (ix:int) = 
         xs.[0..(ix-1)] @ xs.[(ix+1).. ((List.length xs) - 1)]
-    let reclaim xs = 
-        xs.[0].[1] :: xsmapped @@ [xs.[last].[1]]
+    let reclaim (ws: int list list) (xs: int list) = 
+        if (List.length ws = 0) then 
+            []
+        else
+            let middles = ws |> List.map (fun e-> e.[1]) 
+            let l = List.last xs
+            let f = xs.[0]
+            let r = f :: (middles @ [l])                    
+            //printfn "reclaim=%A" r
+            r
+
     let ws = List.windowed 3 xs
-    let toRemoveIxOpt = ws |> List.tryFindIndex (fun (a,b,c) -> a = 0 && b = 0)
+    
+    let toRemoveIxOpt = ws |> List.tryFindIndex (fun w -> w.[0] = 0 && w.[2] = 0)
     match toRemoveIxOpt with
-        | Some ix -> shorten (reclaim(except ws ix))
+        | Some ix -> 
+                let exc = except ws ix
+                //printfn "except=%A" exc
+                shorten (reclaim exc xs)
         | None -> xs
         
 
-
-
-
+arrays
+|> List.iter (fun xs -> 
+                let diff = (List.length xs) - List.length (shorten xs)
+                if diff % 2 = 0 then printfn "%s" "Bob" else printfn "%s" "Alice")
